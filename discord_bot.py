94bd9970dc17c4033a56cc2f7b569c93a6de587a
Minjae Joe(Cho) <ussr1285@ajou.ac.kr>
2023-02-18 16:35:29 +0900

    json 방식의 주식 정보 입출력 & 디스코드 구현

--- discord_bot.py
+++ discord_bot.py
@@ -1,28 +1,134 @@
-# discord bot 테스트
-import discord
-import json
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open("discord_bot_token.json", "r") as json_file:
+# discord bot (stock.py와 따로 동작.)
+import os
+import time
+import json
+import nest_asyncio 
+import discord
+from discord.ext import commands
+
+def load_json_config(stock_code): # 해당 주식이 어떤 설정을 했는지 stock_order_configs 폴더에 있는 파일들에서 가져와서, 주식 객체 만들어줌.
+    # try:
+    with open("./stock_order_configs/"+stock_code+".json", "r") as json_file:
+        json_data = json.load(json_file)
+    return json_data
+    # except:
+    #    return []
+
+def save_json_config(stock_code, max_stock_amount, will_buy, target_buy_price, buy_amount, buy_frequency_time, buy_last_time, will_sell, target_sell_price, sell_amount, sell_frequency_time, sell_last_time): # 각 주식에 수정 사항 있는데로 파일 저장.
+    stock_object = {
+        "stock_code" : stock_code,
+        "max_stock_amount" : max_stock_amount,
+        "will_buy" : will_buy,
+        "target_buy_price" : target_buy_price,
+        "buy_amount" : buy_amount,
+        "buy_frequency_time" : buy_frequency_time,
+        "buy_last_time" : buy_last_time,
+        "will_sell" : will_sell,
+        "target_sell_price" : target_sell_price,
+        "sell_amount" : sell_amount,
+        "sell_frequency_time" : sell_frequency_time,
+        "sell_last_time" : sell_last_time
+    }
+    with open("./stock_order_configs/"+stock_code+".json", "w") as json_file:
+        json.dump(stock_object, json_file)
+
+nest_asyncio.apply()
+
+with open("./discord_bot_token.json", "r") as json_file:
     json_data = json.load(json_file)
 token = json_data['token']
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='.', intents=intents)
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+@bot.command()
+async def ping(ctx, *args):
+    await ctx.send('pong '+ str(args)) # 일단 *args로 여러개 받아올 수는 있는 듯.
 
-@client.event
-async def on_message(message):
-    # print(dir(message))
-    print(message.content)
-    if message.author == client.user:
-        return
+@bot.command()
+async def h(ctx):
+    await ctx.send('사용방법 알려주는 내용') # 사용방법 알려주는 help
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def status(ctx, stock_code): # 주식 매수/매도 설정, 현황 확인
+    origin = load_json_config(stock_code)
+    await ctx.send("종목코드: "+ origin['stock_code']+ "\n최대 한도 매수 주식 개수: "+ str(origin['max_stock_amount'])+ "\n매수 자동화 작동: "+ str(origin['will_buy'])+ "\n목표 매수 주가: "+ str(origin['target_buy_price'])+ "\n매수할 개수: "+ str(origin['buy_amount'])+ "\n매수할 시간 간격: "+ str(origin['buy_frequency_time'])+ "\n매도 자동화 작동: "+ str(origin['will_sell'])+ "\n목표 매도 주가: "+ str(origin['target_sell_price'])+ "\n매도할 주식 개수: "+ str(origin['sell_amount'])+ "\n매도할 시간 간격: "+ str(origin['sell_frequency_time']))
+    
+    
+@bot.command()
+async def init(ctx, stock_code, max_stock_amount, will_buy, target_buy_price, buy_amount, buy_frequency_time, will_sell, target_sell_price, sell_amount, sell_frequency_time):
+    save_json_config(stock_code, max_stock_amount, will_buy, target_buy_price, buy_amount, buy_frequency_time, time.time(), will_sell, target_sell_price, sell_amount, sell_frequency_time, time.time())
+    await ctx.send(stock_code + '에 대한 주식 매매 설정 초기화 완료.')
 
-client.run(token)
+@bot.command()
+async def buy(ctx, stock_code, max_stock_amount, will_buy, target_buy_price, buy_amount, buy_frequency_time): # 매수 설정
+    origin = load_json_config(stock_code)
+    save_json_config(stock_code, max_stock_amount, will_buy, target_buy_price, buy_amount, buy_frequency_time, origin['buy_last_time'], origin['will_sell'], origin['target_sell_price'], origin['sell_amount'], origin['sell_frequency_time'], origin['sell_last_time'])
+    await ctx.send('매수 설정하는 코드')
+
+@bot.command()
+async def sell(ctx, stock_code, will_sell, target_sell_price, sell_amount, sell_frequency_time): # 매도 설정
+    origin = load_json_config(stock_code)
+    save_json_config(stock_code, origin['max_stock_amount'], origin['will_buy'], origin['target_buy_price'], origin['buy_amount'], origin['buy_frequency_time'], origin['buy_last_time'], will_sell, target_sell_price, sell_amount, sell_frequency_time, origin['sell_last_time'])
+    await ctx.send('매도 설정하는 코드')
+
+@bot.command()
+async def modify_max_stock_amount(ctx, stock_code, max_stock_amount):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], max_stock_amount, origin['will_buy'], origin['target_buy_price'], origin['buy_amount'], origin['buy_frequency_time'], origin['will_sell'], origin['target_sell_price'], origin['sell_amount'], origin['sell_frequency_time'])
+    await ctx.send("max_stock_amount: ", max_stock_amount)
+
+@bot.command()
+async def modify_will_buy(ctx, stock_code, will_buy):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], will_buy, origin['target_buy_price'], origin['buy_amount'], origin['buy_frequency_time'], origin['will_sell'], origin['target_sell_price'], origin['sell_amount'], origin['sell_frequency_time'])
+    await ctx.send("will_buy: ", will_buy)
+
+@bot.command()
+async def modify_target_buy_price(ctx, stock_code, target_buy_price):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], origin['will_buy'], target_buy_price, origin['buy_amount'], origin['buy_frequency_time'], origin['will_sell'], origin['target_sell_price'], origin['sell_amount'], origin['sell_frequency_time'])
+    await ctx.send("target_buy_price: ", target_buy_price)
+
+@bot.command()
+async def modify_buy_amount(ctx, stock_code, buy_amount):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], origin['will_buy'], origin['target_buy_price'], buy_amount, origin['buy_frequency_time'], origin['will_sell'], origin['target_sell_price'], origin['sell_amount'], origin['sell_frequency_time'])
+    await ctx.send("buy_amount: ", buy_amount)
+
+@bot.command()
+async def modify_buy_frequency_time(ctx, stock_code, buy_frequency_time):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], origin['will_buy'], origin['target_buy_price'], origin['buy_amount'], buy_frequency_time, origin['will_sell'], origin['target_sell_price'], origin['sell_amount'], origin['sell_frequency_time'])
+    await ctx.send("buy_frequency_time: ", buy_frequency_time)
+
+@bot.command()
+async def modify_will_sell(ctx, stock_code, will_sell):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], origin['will_buy'], origin['target_buy_price'], origin['buy_amount'], origin['buy_frequency_time'], will_sell, origin['target_sell_price'], origin['sell_amount'], origin['sell_frequency_time'])
+    await ctx.send("will_sell: ", will_sell)
+
+@bot.command()
+async def modify_target_sell_price(ctx, stock_code, target_sell_price):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], origin['will_buy'], origin['target_buy_price'], origin['buy_amount'], origin['buy_frequency_time'], origin['will_sell'], target_sell_price, origin['sell_amount'], origin['sell_frequency_time'])
+    await ctx.send("target_sell_price: ", target_sell_price)
+
+@bot.command()
+async def modify_sell_amount(ctx, stock_code, sell_amount):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], origin['will_buy'], origin['target_buy_price'], origin['buy_amount'], origin['buy_frequency_time'], origin['will_sell'], origin['target_sell_price'], sell_amount, origin['sell_frequency_time'])
+    await ctx.send("sell_amount: ", sell_amount)
+
+@bot.command()
+async def modify_sell_frequency_time(ctx, stock_code, sell_frequency_time):
+    origin = load_json_config(stock_code)
+    save_json_config(origin['stock_code'], origin['max_stock_amount'], origin['will_buy'], origin['target_buy_price'], origin['buy_amount'], origin['buy_frequency_time'], origin['will_sell'], origin['target_sell_price'], origin['sell_amount'], sell_frequency_time)
+    await ctx.send("sell_frequency_time: ", sell_frequency_time)
 
 
-
+bot.run(token)
--- stock.py
+++ stock.py
@@ -1,20 +1,76 @@
 # 프로그램 코드 (키움 증권 연동)
 from pykiwoom.kiwoom import *
+import time
+import pandas as pd
+import json
+
 
 # 로그인
 kiwoom = Kiwoom()
-kiwoom.CommConnect(block=True)
-
-# 프로그램 코드 (초기값 및 함수 정의)
+kiwoom.CommConnect(block=True) # 지속적으로 연결이 안되는건가? 주식 거래 체결하고나면 다시 거래 안되고, 이것 다시 실행해서 제연결해주면 됨.
 
 # 계좌번호
-account_list = kiwoom.GetLoginInfo("ACCNO")
-account = account_list[0]
+account = kiwoom.GetLoginInfo("ACCNO")[0]
 
 print(account)
 print(kiwoom.GetConnectState())
 
-def get_wallet(): # 예수금 확인 함수
+# 프로그램 코드 (초기값 및 함수 정의)
+
+# 파일 입출력 json으로 쓴다면..
+
+def load_json_config(): # 각 주식별로 어떤 설정을 했는지 config.json 파일에서 가져와서, 주식 객체 만들어줌.
+    stocks = []
+
+    for stock in json_stocks:
+        with open("./stock_order_configs/"+stock.stock_code, "r") as json_file:
+            json_data = json.load(json_file)
+        stocks.append(
+            Stock(
+                json_data['stock_code'],
+                json_data['max_stock_amount'],
+                json_data['target_buy_price'],
+                json_data['buy_amount'],
+                json_data['buy_frequency_time'],
+                json_data['target_sell_price'],
+                json_data['sell_amount'],
+                json_data['sell_frequency_time']
+            )
+        )
+    return stocks
+
+def save_json_config(stocks): # 각 주식에 수정 사항 있는데로 파일 저장. # db로 하는게 나을 듯..?
+    for stock in stocks:
+        stock_object = {
+            "stock_code" : stock.stock_code,
+            "max_stock_amount" : stock.max_stock_amount,
+            "will_buy" : stock.will_buy,
+            "target_buy_price" : stock.target_buy_price,
+            "buy_amount" : stock.buy_amount,
+            "buy_frequency_time" : stock.buy_frequency_time,
+            "buy_last_time" : stock.buy_last_time,
+            "will_sell" : stock.will_sell,
+            "target_sell_price" : stock.target_sell_price,
+            "sell_amount" : stock.sell_amount,
+            "sell_frequency_time" : stock.sell_frequency_time,
+            "sell_last_time" : stock.sell_last_time
+        }
+        with open("./stock_order_configs/"+stock.stock_code, "w") as json_file:
+            json.dump(stock_object, json_file)
+
+
+def time_distance(input_time):
+    return time.time() - input_time
+
+def get_stock_price(stock_code): # 주식 현재가 확인 함수
+    stock_df = kiwoom.block_request("opt10001",
+        종목코드=stock_code,
+        output="주식기본정보",
+        next=0
+    )
+    return abs(int(stock_df['현재가'])) # 하락하고 있으면 음수로 반환해서 abs함수로 절대값으로 만듬.
+
+def get_wallet(): # 주문 가능 금액(예수금) 확인 함수
     # opw00001 요청
     wallet_df = kiwoom.block_request("opw00001",
         계좌번호=account,
@@ -24,39 +80,92 @@ def get_wallet(): # 예수금 확인 함수
         output="예수금상세현황",
         next=0
     )
-    print(wallet_df['예수금'][0]) # 에러 확인
-    money = int(wallet_df['예수금'][0])
+    money = int(wallet_df['주문가능금액'][0])
     return money
 
-def get_stock_price(stock_code): # 주식 현재가 확인 함수
-    stock_df = kiwoom.block_request("opt10001",
-        종목코드=stock_code,
-        output="주식기본정보",
-        next=0
-    )
-    print(stock_df['현재가']) # 에러 확인
-    price = int(stock_df['현재가'])
-    return price
-    
-def buy_stock(stock_code, target_price, how_many_amount, wallet): # 주식 매수 함수
-    stock_price = get_stock_price(stock_code)
-    if((wallet >= stock_price*how_many_amount) and (stock_price <= target_price)):
-        # 시장가주문 매수
-        kiwoom.SendOrder("시장가매수", "0101", account, 1, stock_code, how_many_amount, 0, "03", "")
+class Stock():
+    def __init__(self, stock_code, max_stock_amount, target_buy_price, buy_amount, buy_frequency_time, target_sell_price, sell_amount, sell_frequency_time):
+        self.stock_code = stock_code # 주식 주문 코드
+        self.stock_price = get_stock_price(self.stock_code) # 주식 현재가
 
-def sell_stock(stock_code, target_price, how_many_amount, stock_amount): # 주식 매도 함수
-    # 시장가주문 매도
-    kiwoom.SendOrder("시장가매도", "0101", account, 2, stock_code, how_many_amount, 0, "03", "")
+        self.stock_amount = self.get_stock_amount() # 보유 주식 수.
+        self.max_stock_amount = max_stock_amount # 최대 매수 가능한 주식 수, 여기에 도달하면 더 이상 매수 안 함.
 
-def trade_function(stock_code): # 거래 결정 함수 (이걸 계속 돌리면 될 듯.)
-    wallet = get_wallet()
-    # stock_amount = 
+        self.will_buy = True # 매수 희망 유무
+        self.target_buy_price = target_buy_price # 목표로 하는 매수가. 이것보다 현재가가 싸면 매수.
+        self.buy_amount = buy_amount # 매수를 할 주식 개수
 
+        self.buy_frequency_time = buy_frequency_time # 초 단위
+        self.buy_last_time = time.time()
+        
+        self.will_sell = True # 매도 희망 유무
+        self.target_sell_price = target_sell_price # 목표로 하는 매도가. 이것보다 현재가가 비싸면 매도.
+        self.sell_amount = sell_amount # 매도를 할 주식 개수
+        self.sell_frequency_time = sell_frequency_time # 초 단위
+        self.sell_last_time = time.time()
+        print(self.stock_code, "주식 객체 생성 완료.")
 
-# 프로그램 코드 (테스트)
-stock_code = "038500"
-how_many_amount = 1
-wallet = get_wallet()
-target_price = 3700
+    def get_stock_amount(self):
+        stock_df = kiwoom.block_request("opw00004",
+            계좌번호=account,
+            비밀번호="",
+            상장폐지조회구분="0",
+            비밀번호입력매체구분="00",
+            output="주식기본정보",
+            next=0
+        )
+        if(stock_df['종목코드'].str.contains(self.stock_code)[0]):
+            target_stock = stock_df[stock_df['종목코드'].str.contains(self.stock_code)] # 종목코드에 해당하는 데이터 행 찾기.
+            return int(target_stock['보유수량']) # int()
+        else:
+            return 0
 
-buy_stock(stock_code, target_price, how_many_amount, wallet)
+        
+    def modify_will_buy(self, modifed_amount):
+        self.will_buy = modifed_amount
+        print("살 주식 수 수정 완료", self.will_buy)
+
+    def modify_will_sell(self, modifed_amount):
+        self.will_sell = modifed_amount
+        print("팔 주식 수 수정 완료", self.will_sell)
+
+    def buy_stock(self): # 주식 매수 함수
+        wallet = get_wallet()
+        stock_price = get_stock_price(self.stock_code)
+        if(
+            wallet >= stock_price*self.buy_amount 
+            and stock_price <= self.target_buy_price
+            and time_distance(self.buy_last_time) >= self.buy_frequency_time
+        ):
+            if(self.max_stock_amount < self.stock_amount + self.buy_amount): # 매수를 하고 싶은데 주식 최대 보유량을 초과한다면 어떻게 할 것인가?에 관한 조건문
+                temp_amount = (self.stock_amount + self.buy_amount) - self.max_stock_amount 
+                if(temp_amount > 0):
+                    kiwoom.SendOrder("시장가매수", "0101", account, 1, self.stock_code, temp_amount, 0, "03", "") # 시장가주문 매수
+                    self.buy_last_time = time.time()
+            else:
+                kiwoom.SendOrder("시장가매수", "0101", account, 1, self.stock_code, self.buy_amount, 0, "03", "") # 시장가주문 매수
+                self.buy_last_time = time.time()
+            print(self.stock_code, "매수")
+            
+    def sell_stock(self): # 주식 매도 함수
+        stock_price = get_stock_price(self.stock_code)
+        if(
+            stock_price >= self.target_sell_price
+            and time_distance(self.sell_last_time) >= self.sell_frequency_time
+        ):
+            if(self.stock_amount < self.sell_amount):
+                if(self.stock_amount > 0):
+                    kiwoom.SendOrder("시장가매도", "0101", account, 2, self.stock_code, self.stock_amount, 0, "03", "") # 시장가주문 매도
+                    self.sell_last_time = time.time()
+            else:
+                kiwoom.SendOrder("시장가매도", "0101", account, 2, self.stock_code, self.sell_amount, 0, "03", "") # 시장가주문 매도
+                self.sell_last_time = time.time()
+            print(self.stock_code, "매도")
+
+# def modify_function(stocks):
+
+def trade_function(stocks): # 거래 결정 함수 (이걸 계속 돌리면 될 듯.)
+    for stock in stocks:
+        stock.buy_stock()
+        time.sleep(1)
+    time.sleep(2)
